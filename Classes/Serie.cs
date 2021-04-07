b04a137f2e98112d6e9203ed93fed9c90967c092
braferraz <braianfs2009@gmail.com>
2021-04-07 15:28:39 -0300

    Implementando o cadastro

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -13,7 +13,7 @@ namespace DIO.Series
         private string Titulo { get; set; }
         private string Descricao { get; set; }
         private int Ano { get; set; }
-
+        private bool Excluido { get; set; }
         // Métodos
 
         public Serie(int id, Genero genero, string titulo, string descricao, int ano)
@@ -23,6 +23,7 @@ namespace DIO.Series
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
@@ -32,6 +33,7 @@ namespace DIO.Series
             retorno += "Título: " + this.Titulo + Environment.NewLine;
             retorno += "Descrição: " + this.Descricao + Environment.NewLine;
             retorno += "Ano de Início: " + this.Ano;
+            retorno += "Excluido: " + this.Excluido;
             return retorno;
         }
 
@@ -43,6 +45,14 @@ namespace DIO.Series
         public int retornaId()
         {
             return this.Id;
+        }
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
         }
     }
 }
