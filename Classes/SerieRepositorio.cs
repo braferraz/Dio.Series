6dfcd43b22be72479c5c881fcb3047404219bd9f
braferraz <braianfs2009@gmail.com>
2021-04-06 22:19:33 -0300

    Adcionando interfaces e repositorios

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using DIO.Series.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.Series
+{
+    class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
